8d8eef10d931ed526e2ee393277e0b0fede84292
xuepengbo <xuepengbo@chinasofti.com>
2018-07-24 11:16:06 +0800

    springcloud断路器仪表盘

---
+++ springCloudHystrixDashboard/src/main/java/com/example/springcloudhystrixdashboard/SpringcloudhystrixdashboardApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudhystrixdashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudhystrixdashboardApplication.class, args);
+    }
+}
