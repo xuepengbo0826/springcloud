ef3e2d6f9eac691890992e4a0ed106f14ace436b
xuepengbo <xuepengbo@chinasofti.com>
2018-07-25 11:50:40 +0800

    springcloud hystrix 仪表盘配置文件修改

--- springCloudHystrixDashboard/src/main/java/com/example/springcloudhystrixdashboard/SpringcloudhystrixdashboardApplication.java
+++ springCloudHystrixDashboard/src/main/java/com/example/springcloudhystrixdashboard/SpringcloudhystrixdashboardApplication.java
@@ -3,13 +3,11 @@ package com.example.springcloudhystrixdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-@EnableEurekaClient
 public class SpringcloudhystrixdashboardApplication {
 
     public static void main(String[] args) {
